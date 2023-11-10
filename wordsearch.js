94764315a01d5cf018e39a785fdae80b4bffaf3c
Matheus-Oliveira-Chaves <Matheus.oliveira.chaves@hotmail.com>
2023-11-09 22:44:38 -0500

    Include vertical search

--- wordsearch.js
+++ wordsearch.js
@@ -3,6 +3,21 @@ const wordSearch = (letters, word) => {
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
-}
+    const verticalJoin = [];
+    for (let i = 0; i < letters[0].length; i++) {
+        let verticalString = '';
+        for (let j = 0; j < letters.length; j++) {
+            verticalString += letters[j][i];
+        }
+        verticalJoin.push(verticalString);
+    }
+
+    for (let l of verticalJoin) {
+        if (l.includes(word)) return true;
+    }
+
+    return false;
+
+};
 
 module.exports = wordSearch
